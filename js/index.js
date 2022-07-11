d0430debba5755f1923736d0e90270accfa58850
Yuk Fai Hsu <yukfai@Yuks-MacBook-Pro.local>
2022-07-11 13:38:39 -0400

    Save the criteria in the landing page to local storage

--- js/index.js
+++ js/index.js
@@ -3,11 +3,16 @@
 
 // ---------------------- Event Handler functions ----------------------
 const buttonClicked = () => {
-    console.log(`Reserve button clicked!`)
+    console.log(`Reserve button clicked!`);
     window.location.href = "./camping-site-page/choose-camping.html";
+
+    const formFields = document.querySelectorAll("select");
+
+    localStorage.setItem("equipment-type-from-screen-1", formFields[0].value);
+    localStorage.setItem("number-of-night-from-screen-1", formFields[1].value);
 }
 
 
 // ---------------------- Event listeners ----------------------
 // listener for the reserve button
-document.querySelector("#camp-page-btn").addEventListener("click", buttonClicked)
+document.querySelector("#camp-page-btn").addEventListener("click", buttonClicked);
